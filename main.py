afda2bd96dd04cedfaf5e4a2b821196f88d0ca93
Ogrebatel <54839638+Ogrebatel@users.noreply.github.com>
2021-02-18 17:52:33 +0300

    THANKS TO https://algorithmica.org/ru/reciprocal
    THANKS TO https://algorithmica.org/ru/reciprocal

--- main.py
+++ main.py
@@ -21,9 +21,10 @@ class EllipticCurve:
         self.p = pIn
 
 
+
 class Point:
-    x = float(0)
-    y = float(0)
+    x = int(0)
+    y = int(0)
     curve = EllipticCurve()
 
     def set(self, xIn: int, yIn: int, cIn: EllipticCurve):
@@ -34,45 +35,47 @@ class Point:
     def print(self):
         print(self.x, self.y)
 
+    def inv(self, x):
+        return pow(x, self.curve.p - 2, self.curve.p)
+
     def __add__(self, other):
         result = Point()
         if self.curve != other.curve:
             return ArithmeticError
+        result.curve = self.curve
+
+        if (self.x == 0) and (self.y == 0):
+            return other
+
+        if (other.x == 0) and (other.y == 0):
+            return self
+
+        if (self.x == other.x) and (self.y == ((-other.y) % other.curve.p)):
+            result.set(0, 0, self.curve)
+            return result
+
         if (self.x == other.x) and (self.y == other.y):
-            alpha = (3 * self.x * self.x + self.curve.a) / (2 * self.y)
+            if self.y == 0:
+                print("gg2")
+            alpha = ((3 * self.x * self.x + self.curve.a) * self.inv(2 * self.y)) % self.curve.p
         else:
-            alpha = (other.y - self.y) / (other.x - self.x)
-        result.x = alpha * alpha - self.x - other.x
-        result.y = -self.y + alpha * (self.x - result.x)
+            alpha = ((other.y - self.y) * self.inv(other.x - self.x)) % self.curve.p
+        result.x = (alpha * alpha - self.x - other.x) % self.curve.p
+        result.y = (-self.y + alpha * (self.x - result.x)) % self.curve.p
         return result
 
-    def __iadd__(self, other):
+    def mul(self, n):
         result = Point()
-        if self.curve != other.curve:
-            return ArithmeticError
-        if (self.x == other.x) and (self.y == other.y):
-            alpha = (3 * self.x * self.x + self.curve.a) / (2 * self.y)
-        else:
-            alpha = (other.y - self.y) / (other.x - self.x)
-        result.x = alpha * alpha - self.x - other.x
-        result.y = -self.y + alpha * (self.x - result.x)
-        self.x = result.x
-        self.t = result.y
-
-
-    # def __mul__(self, other):
-    #     if type
-    #     result = Point()
-    #     if self.curve != other.curve:
-    #         return ArithmeticError
-    #     if (self.x == other.x) and (self.y == other.y):
-    #         alpha = (3 * self.x * self.x + self.curve.a) / (2 * self.y)
-    #     else:
-    #         alpha = (other.y - self.y) / (other.x - self.x)
-    #     result.x = alpha * alpha - self.x - other.x
-    #     result.y = -self.y + alpha * (self.x - result.x)
-    #     return result
-
+        result.set(0, 0, self.curve)
+        m2 = self
+        while n > 0:
+            if n & 1 == 1:
+                result = result + m2
+                pass
+            n = n >> 1
+            m2 = m2 + m2
+            pass
+        return result
 
 class ECP:
     q = int(0)
@@ -84,20 +87,16 @@ class ECP:
 
     def __init__(self, PIn: Point, QIn: Point, cIn: EllipticCurve, qIn: int):
         self.P = PIn
-        self.P.print()
         self.q = qIn
         self.curve = cIn
         self.Q = QIn
 
-
     def signature(self, d: int):
         r = int(0)
-        C = Point()
-        C.set(0, 0, self.curve)
         s = int(0)
 
-        #alpha = hash(msg)
-        e = 0x2DFBC1B372D89A1188C09C52E0EEC61FCE52032AB1022E8E67ECE6672B043EE5#e = alpha % q
+        # alpha = hash(msg)
+        e = 0x2DFBC1B372D89A1188C09C52E0EEC61FCE52032AB1022E8E67ECE6672B043EE5  # e = alpha % q
         if (e == 0):
             e = 1
 
@@ -105,17 +104,14 @@ class ECP:
 
         while (s == 0):
             while (r == 0):
-                C.set(0, 0, self.curve)
-                for i in range(k):
-                    C = C + self.P
+                C = P.mul(k)
                 r = C.x % self.q
-            C.print()
             s = (r * d + k * e) % self.q
+            print(s)
         return str(r) + str(s)
 
 
 if __name__ == '__main__':
-
     # Заполнение параметров Эллитической кривой, Точек, q,
     curve = EllipticCurve()
     curve.set(7, 0x5FBFF498AA938CE739B8E022FBAFEF40563F6E6A3472FC2A514C0CE9DAE23B7E,
@@ -131,39 +127,21 @@ if __name__ == '__main__':
     Q = Point()
     Q.set(0x7F2B49E270DB6D90D8595BEC458B50C58585BA1D4E9B788F6689DBD8E56FD80B,
           0x26F1B489D6701DD185C8413A977B3CBBAF64D1C593D26627DFFB101A87FF77DA, curve)
-    C = Q + P
-    C.print()
+
     e = 0x2DFBC1B372D89A1188C09C52E0EEC61FCE52032AB1022E8E67ECE6672B043EE5
     k = 0x77105C9B20BCD3122823C8CF6FCC7B956DE33814E95B7FE64FED924594DCEAB3
     d = int(0x7A929ADE789BB9BE10ED359DD39A72C11B60961F49397EEE1D19CE9891EC3B28)
     test = ECP(P, Q, curve, q)
 
-    test.signature(d)
+    print(test.signature(d))
 
-
-    #test.signature(d)
-
-"""
-    test = int(input("enter value: "))
-    test += 5
-    if test == 15:
-        print("число = 15")
-    if type(test) == int:
-        print("input - int")
-
-    print(test)
-
-    d1 = []
-    d1 = [1, 2, 3, 4, 5, 6]
-    d2 = [8, 9, 10]
-    d1.append(7)
-    d1.extend(d2)
-    big = 123123123123123123321312323123123123
-
-    print(big)
-    for j in range(5):
-        d2 = []
-        for i in range(5):
-            d2.append(0)
-        d1.append(d2)
-"""
+    '''
+    xp = 2
+    yp = 4018974056539037503335449422937059775635739389905545080690979365213431566280
+    
+    xq = 57520216126176808443631405023338071176630104906313632182896741342206604859403
+    yq = 17614944419213781543809391949654080031942662045363639260709847859438286763994
+    
+    р =  57896044618658097711785492504343953926634992332820282019728792003956564821041
+    k =  53854137677348463731403841147996619241504003434302020712960838528893196233395    
+    '''
